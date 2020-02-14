b42434913ce9105f0899ed5a85144d6316f963f9
al1nk <jmarc.fares@gmail.com>
2020-02-14 18:10:08 +0100

    disambiguated StructName/Decl, FunDecl and VarDecl, added optionnals where appropriated and replaced type vtype.None with vtype.Null

--- ast.mli
+++ ast.mli
@@ -1,18 +1,19 @@
 type bop = Plus | Minus | Mult | Div | Mod
 type uop = Pos | Neg | Ptr
-type vtype = Void | Int | Float | String | Char | Struct of string
+
+type vtype = Void | Int | Float | String | Char | StructName of string
 
 type expr = Ident of string
   | Cst of vtype * string
   | Binop of expr * bop * expr
   | Unop of uop * expr
-  | None
+  | Null
 
 type param = Param of vtype * string
 
-type decl = 
-    Var of vtype * (string * expr) list
-    | Fun of string * vtype * param list * decl list
+type decl = VarDecl of vtype * (string * (expr option)) list
+    | FunDecl of vtype * string * param list option * decl list option
+    | StructDecl of string * (decl list option)
     | DeclError
 
 type file = File of decl list
