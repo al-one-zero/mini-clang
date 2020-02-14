fa7a433377ddce1c4057cf7859d300c6db96c80c
al1nk <jmarc.fares@gmail.com>
2020-02-14 15:18:10 +0100

    added types for complete variable declaration

--- ast.mli
+++ ast.mli
@@ -2,9 +2,17 @@ type bop = Plus | Minus | Mult | Div | Mod
 type uop = Pos | Neg | Ptr
 type vtype = Void | Int | Float | String | Char | Struct of string
 
-type expr = Var of string * vtype * string 
-  | Cst of string * vtype * string 
+type expr = Ident of string
+  | Cst of vtype * string
   | Binop of expr * bop * expr
   | Unop of uop * expr
-type decl = Expr of expr | ExprError
+  | None
+
+type param = Param of vtype * string
+
+type decl = 
+    Var of vtype * (string * expr) list
+    | Fun of string * vtype * param list * decl list
+    | DeclError
+
 type file = File of decl list
